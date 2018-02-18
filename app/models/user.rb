4c4843b0b8230a982069423e132aa798afe69dc6
noordean <ebroyeem90@gmail.com>
2018-02-18 18:07:21 +0100

    feat(registration): add functionality for registration
    - add bcrypt gem
    - create association between users and other tables
    
    [Delivers #155312488]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  has_many :urls
+
+  validates :username, uniqueness: { case_sensitive: false }, format: {
+    with: /\A[a-zA-Z]{5,15}\z/,
+    message: "must contain letters and be between 5-15 characters"
+  }
+  validates :password, format: {
+    with: /\A^(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z\d]{5,12}\z/,
+    message: "must be alphanumerics and between 5-12 characters"
+  }
+  validates :email, uniqueness: true, format: {
+    with: /\A[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/,
+    message: "must be valid"
+  }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when validating fields" do
+    # it { is_expected.to validate_presence_of(:username) }
+    # it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_uniqueness_of(:username).case_insensitive }
+    it { is_expected.to validate_uniqueness_of(:email) }
+  end
+
+  context "when validating associations" do
+    it { is_expected.to have_many(:urls) }
+  end
 end
