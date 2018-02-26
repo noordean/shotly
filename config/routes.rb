226504673d8a2942556e4d88b0b5fd3ebf1e1cc5
noordean <ebroyeem90@gmail.com>
2018-02-26 13:45:23 +0100

    feat(urlLocations): get url's locations
    - get all locations from which a url has been visited
    
    [Delivers #155312503]

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   resources :users
   post "/login" => "users#login"
   get "/user/urls" => "urls#get_user_urls"
+
+  get "/url/:id/locations" => "locations#get_url_locations"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
